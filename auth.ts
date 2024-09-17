f8149f2e7feab9711996f1baa2a517d60eb59445
Tom Davies <tomdavies@McBk-0148.local>
2024-09-17 11:14:29 +0100

    refactor

--- auth.ts
+++ auth.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-// import { authConfig } from "./auth.config";
 
 export const { auth, signIn, signOut } = NextAuth({
-  // ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,4 @@
-import NextAuth from "next-auth";
-import { auth } from "./auth";
-
-export default auth;
+export { auth as middleware } from "@/auth";
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
