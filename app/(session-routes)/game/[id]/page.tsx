e42c94198776c5a9a0ee5213fe93c25935fac6a4
Tom Davies <tadavies1988@gmail.com>
2025-04-19 17:14:03 +0100

    quickfix

--- app/(session-routes)/game/[id]/page.tsx
+++ app/(session-routes)/game/[id]/page.tsx
@@ -4,7 +4,11 @@ import { fetchGame } from '@/app/api/data/server/game';
 import { GameWrapper } from '@/app/components/game-wrapper';
 import Game from '@/app/components/game/game';
 
-export default async function GamePage({ params }: { params: { id: string } }) {
+export default async function GamePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const game: Fetched<Game> = await fetchGame(id);
 
