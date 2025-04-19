33f5f1cdb6261b2c9cc082d9a03463310b2ae958
Tom Davies <tadavies1988@gmail.com>
2025-04-19 18:42:10 +0100

    amended type error

--- app/store-provider.tsx
+++ app/store-provider.tsx
@@ -1,22 +1,32 @@
-"use client";
-import { useRef } from "react";
-import { Provider } from "react-redux";
-import { makeStore, AppStore } from "@/app/redux/store";
-import { Session } from "next-auth";
-import { setUser } from "./redux/slices/user-slice";
+'use client';
+import { useRef } from 'react';
+import { Provider } from 'react-redux';
+import { makeStore, AppStore } from '@/app/redux/store';
+import { Session } from 'next-auth';
+import { setUser } from './redux/slices/user-slice';
 
 export default function StoreProvider({
   user,
   children,
 }: {
-  user: Fetched<Session["user"]>;
+  user: Fetched<Session['user']>;
   children: React.ReactNode;
 }) {
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
-    storeRef.current.dispatch(setUser(user));
+    storeRef.current.dispatch(
+      setUser(
+        user ?? {
+          id: '',
+          name: '',
+          email: '',
+          token: '',
+          premium: false,
+        }
+      )
+    );
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
